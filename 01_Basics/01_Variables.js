992a842059ae4254274f7d12443f145e84a6f7d6
Nayan Kunwar <nayankunwar678@gmail.com>
2023-12-02 12:27:50 +0530

    Datatypes

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -1,1 +1,16 @@
+// 1 - Variables
 const accountId = 101;
+let accountEmail = "nayan@gmail.com";
+var accountPassword = 123;
+accountCity = "Dehradun";
+let test;
+
+//accountId = 102; error cannot change
+
+accountEmail = "nk@gmail.com";
+accountPassword = 321;
+accountCity = "Rudraprayag";
+/*
+Not use var. Because of issue in block scope and functional scope.
+*/ 
+console.table([accountId, accountEmail, accountPassword, accountCity, test]);
